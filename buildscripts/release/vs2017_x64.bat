7dd853de14e23b157d3d472fd36817945238a198
Pugachev Andrey <apugachevdev@gmail.com>
2019-10-06 15:13:08 +0300

    Removed unneccessary build scripts

--- buildscripts/release/vs2017_x64.bat
+++ buildscripts/release/vs2017_x64.bat
@@ -4,7 +4,6 @@ cd ..
 mkdir build
 cd build
 del CMakeCache.txt
-set TESTS=enabled
 cmake -G "Visual Studio 15 2017 Win64" .. -DMSVC_RUNTIME=dynamic -DCMAKE_BUILD_TYPE=Release -DCMAKE_USE_PTHREADS_INIT=1
 cd ..
 cd ./buildscripts/release/
