e2a8d66b2c26b8faaa8b45449622870caeb73075
Pugachev Andrey <apugachevdev@gmail.com>
2019-10-06 15:00:18 +0300

    Building on Windows

--- buildscripts/debug/vs2017_x64.bat
+++ buildscripts/debug/vs2017_x64.bat
@@ -4,6 +4,6 @@ cd ..
 mkdir build
 cd build
 del CMakeCache.txt
-cmake -G "Visual Studio 15 2017 Win64" .. -DMSVC_RUNTIME=dynamic -DCMAKE_BUILD_TYPE=Debug -DTESTS=true
+cmake -G "Visual Studio 15 2017 Win64" .. -DMSVC_RUNTIME=dynamic -DCMAKE_BUILD_TYPE=Debug -DTESTS=true -DCMAKE_USE_PTHREADS_INIT=1
 cd ..
 cd ./buildscripts/debug/
--- buildscripts/release/vs2017_x64.bat
+++ buildscripts/release/vs2017_x64.bat
@@ -5,6 +5,6 @@ mkdir build
 cd build
 del CMakeCache.txt
 set TESTS=enabled
-cmake -G "Visual Studio 15 2017 Win64" .. -DMSVC_RUNTIME=dynamic -DCMAKE_BUILD_TYPE=Release
+cmake -G "Visual Studio 15 2017 Win64" .. -DMSVC_RUNTIME=dynamic -DCMAKE_BUILD_TYPE=Release -DCMAKE_USE_PTHREADS_INIT=1
 cd ..
 cd ./buildscripts/release/
